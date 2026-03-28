abaed81906f6c9d47e4c1bece6be5b6a25d9ecca
punch2605 <117850036+punch2605@users.noreply.github.com>
2026-03-28 12:31:59 +0700

    Update_chibao
    Added fields and methods for name, age, and GPA management.

--- luyen_tap/src/luyen_tap/sum1n.java
+++ luyen_tap/src/luyen_tap/sum1n.java
@@ -1,10 +1,46 @@
 package luyen_tap;
 
 public class sum1n {
+    private String name;
+    private int age;
+    private double gpa;
+
+    public baitapaa(String name, int age, double gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name + ", Age: " + age + ", GPA: " + gpa);
+    }
 
 	public static void main(String[] args) {
-		System.out.println("hello word");
-
+		
 	}
 
 }
