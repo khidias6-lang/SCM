66d6ace54ac13761f0e35b8a60f7060743e29a24
PC <PC@172.25.20.210>
2026-03-28 11:22:03 +0700

    BƯỚC 1

---
+++ .svn/pristine/ab/ab9876ca5a9b20c3fee7ea96ab746adbf9e4e9a6.svn-base
@@ -0,0 +1,10 @@
+package luyen_tap;
+
+public class sum1n {
+
+	public static void main(String[] args) {
+		System.out.println("hello word");
+
+	}
+
+}
---
+++ luyen_tap/src/luyen_tap/test.java
@@ -0,0 +1,10 @@
+package luyen_tap;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
